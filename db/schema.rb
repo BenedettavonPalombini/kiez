b152a0353ee40fc1cd48c0fa19f58a7b8e562d0d
Edward Leslie <ejleslie@buffalo.edu>
2022-11-30 15:08:11 +0100

    Geocoding... hopefully it works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_161337) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_102031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,8 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_161337) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
     t.string "kind"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,7 +60,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_161337) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.boolean "verified", default: true
+    t.boolean "verified", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
