63e655047f65d32664823bf5a4459142107a91ce
Edward Leslie <ejleslie@buffalo.edu>
2022-12-02 16:29:50 +0100

    you can write messages now

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :conversations, only: [:show, :index] do
+  resources :conversations, only: [:create, :show, :index] do
     resources :messages, only: :create
   end
 
