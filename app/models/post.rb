ff44b19c46d582c1d6660740a5b660b255d6fac3
Edward Leslie <ejleslie@buffalo.edu>
2022-12-06 11:20:24 +0100

    still working

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :category, presence: true
-
   has_many :bookmarks, dependent: :destroy
   validates :kind, inclusion: { in: ["neighborhood", "building"] }
   KINDS = ["neighborhood", "building"]
